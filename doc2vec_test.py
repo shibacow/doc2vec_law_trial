1f1ba2d604eb20fd0b847d3c22e8feb84905ea87
shibacow <shibacow@gmail.com>
2017-06-13 19:10:23 +0000

    ch data

--- doc2vec_test.py
+++ doc2vec_test.py
@@ -11,8 +11,10 @@ import re
 import logging
 from pyquery import PyQuery as pq
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-OUTPUT_MODEL = 'doc2vec.model.laws.gaiji'
-PASSING_PRECISION = 93
+OUTPUT_MODEL = 'doc2vec.model.laws.keiji'
+SAMPLING=500
+PASSING_PRECISION = int(0.95*SAMPLING)
+import random
 
 class MongoOp(object):
     def __init__(self,host='localhost'):
@@ -26,7 +28,8 @@ def connect_mongo():
     return MongoOp('localhost')
 
 class OneLaw(object):
-    def __init__(self,tagger,elm):
+    def __init__(self,tagger,elm,count):
+        self.count=count
         self.tagger=tagger
         self.body=elm['body']
         self.title=elm['title']
@@ -38,28 +41,32 @@ class OneLaw(object):
         d=pq(body)
         return d('body').text()
     def __tokenized(self,body):
-        lines=self.tagger.parse(body).splitlines()
+        ps=self.tagger.parse(body)
+        if not ps:
+            return None
+        lines=ps.splitlines()
         words = []
         for line in lines:
             chunks = line.split('\t')
             if len(chunks) > 3 and (chunks[3].startswith('動詞') or chunks[3].startswith('形容詞') or (chunks[3].startswith('名詞') and not chunks[3].startswith('名詞-数'))):
                 words.append(chunks[0])
-        logging.info("name={} word={}".format(self.title,words[:200]))
+        logging.info("count={} name={} word={}".format(self.count,self.title,words[:20]))
         return TaggedDocument(words=words, tags=[self.title])
 
+                    
 def train(sentences):
     model = models.Doc2Vec(size=400, alpha=0.0015, sample=1e-4, min_count=1, workers=4)
     model.build_vocab(sentences)
-    for x in range(100):
-        print(x)
+    for x in range(30):
+        logging.info("x={}".format(x))
         model.train(sentences)
         ranks = []
-        for doc_id in range(50):
+        for doc_id in range(SAMPLING):
             inferred_vector = model.infer_vector(sentences[doc_id].words)
             sims = model.docvecs.most_similar([inferred_vector], topn=len(model.docvecs))
             rank = [docid for docid, sim in sims].index(sentences[doc_id].tags[0])
             ranks.append(rank)
-        print(collections.Counter(ranks))
+        logging.info(collections.Counter(ranks))
         if collections.Counter(ranks)[0] >= PASSING_PRECISION:
             break
     return model
@@ -68,9 +75,11 @@ def main():
     mp=connect_mongo()
     tagger = MeCab.Tagger('-Ochasen')
     sentences=[]
-    for a in mp.base.find({'cat':"外事"}):
-        ol=OneLaw(tagger,a)
-        sentences.append(ol.sentence)
-    train(sentences)
+    for i,a in enumerate(mp.base.find()):
+        ol=OneLaw(tagger,a,i)
+        if ol.sentence:
+            sentences.append(ol.sentence)
+    #random.shuffle(sentences)
+    model=train(sentences)
     model.save(OUTPUT_MODEL)
 if __name__=='__main__':main()
